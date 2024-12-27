cd52a24cba52c429eae2ca96f5e06a7abc4d7237
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-12-27 16:04:34 +0000

    edited admin to display all attributes, including id

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import PageView, User, Hobbies, UserHobby, Friendship
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'dob', 'is_staff', 'is_active')
+
+class PageViewAdmin(admin.ModelAdmin):
+    list_display = ('count',)
+
+class HobbiesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+class UserHobbyAdmin(admin.ModelAdmin):
+    list_display = ('user', 'hobby')
+
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'friend', 'status', 'accepted', 'created_at')
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(PageView)
-admin.site.register(Hobbies)
-admin.site.register(UserHobby)
-admin.site.register(Friendship)
+admin.site.register(User, UserAdmin)
+admin.site.register(PageView, PageViewAdmin)
+admin.site.register(Hobbies, HobbiesAdmin)
+admin.site.register(UserHobby, UserHobbyAdmin)
+admin.site.register(Friendship, FriendshipAdmin)
