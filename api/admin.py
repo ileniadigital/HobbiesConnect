2e5146b687de1b6a22bc82546627b090ab565064
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-12-27 15:52:48 +0000

    created superuser for admin and adjusted admin file to register all the models

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import PageView, User
+from .models import PageView, User, Hobbies, UserHobby, Friendship
 # Register your models here.
 admin.site.register(User)
 admin.site.register(PageView)
+admin.site.register(Hobbies)
+admin.site.register(UserHobby)
+admin.site.register(Friendship)
--- api/models.py
+++ api/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-
-
 class PageView(models.Model):
     count = models.IntegerField(default=0)
 
