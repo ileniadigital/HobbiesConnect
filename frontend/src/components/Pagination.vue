12cd892271de71e40264cc24ae9350207cea3507
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-12-30 17:19:36 +0000

    Added pagination component

---
+++ frontend/src/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item" :class="{ disabled: currentPage === 1 }">
+                <a class="page-link" href="#" @click.prevent="changePage(currentPage - 1)">Previous</a>
+            </li>
+            <li class="page-item" v-for="page in totalPages" :key="page" :class="{ active: currentPage === page }">
+                <a class="page-link" href="#" @click.prevent="changePage(page)">{{ page }}</a>
+            </li>
+            <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+                <a class="page-link" href="#" @click.prevent="changePage(currentPage + 1)">Next</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        currentPage: {
+            type: Number,
+            required: true
+        },
+        totalPages: {
+            type: Number,
+            required: true
+        }
+    },
+    emits: ["page-changed"],
+    methods: {
+        changePage(page: number) {
+            if (page > 0 && page <= this.totalPages) {
+                this.$emit("page-changed", page);
+            }
+        }
+    }
+});
+</script>
+
+<style scoped></style>
