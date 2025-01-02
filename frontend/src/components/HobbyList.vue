4c3e10e4c6b9d81f053710c17b1b635c6984992e
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-02 17:47:32 +0000

    created hobby list component

---
+++ frontend/src/components/HobbyList.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2>Hobbies</h2>
+    <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    data() {
+        return {
+            hobbies: [
+                { name: "Reading" },
+                { name: "Swimming" },
+                { name: "Coding" }
+            ],
+        }
+    }
+})
+</script>
+
+<style scoped></style>
--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -10,30 +10,30 @@
   </div>
   <div class="row">
     <div class="col-md-12">
-        <form>
-          <div class="form-row vertical">
-            <div class="form-field">
-              <label for="dob">Date of Birth:</label>
-              <input type="date" v-model="dateofbirth" id="dob">
-            </div>
-            <div class="form-field">
-              <label for="name">Name</label>
-              <input type="text" v-model="name" id="name">
-            </div>
-            <div class="form-field">
-              <label for="email">Email</label>
-              <input type="email" v-model="email" id="email">
-            </div>
-            <button type="submit" class="btn btn-primary" v-on:click="">Update Profile</button>
+      <form>
+        <div class="form-row vertical">
+          <div class="form-field">
+            <label for="dob">Date of Birth:</label>
+            <input type="date" v-model="dateofbirth" id="dob">
           </div>
-          
-        </form>
+          <div class="form-field">
+            <label for="name">Name</label>
+            <input type="text" v-model="name" id="name">
+          </div>
+          <div class="form-field">
+            <label for="email">Email</label>
+            <input type="email" v-model="email" id="email">
+          </div>
+          <button type="submit" class="btn btn-primary" v-on:click="">Update Profile</button>
+        </div>
+
+      </form>
     </div>
     <div>
       <button type="button" class="btn btn-primary">Update Password</button>
     </div>
   </div>
-  <div class="container">
+  <!-- <div class="container">
     <div class="row">
       <div class="col-md-12">
         <div class="hobbies">
@@ -54,25 +54,26 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
+  <HobbyList />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Header from "../components/Header.vue";
+import HobbyList from "../components/HobbyList.vue";
 
 export default defineComponent({
   components: {
-    Header
+    HobbyList,
   },
   data() {
     return {
       title: "Profile",
       username: "Amy123",
       hobbies: [
-        {name: "Reading"}, 
-        {name: "Swimming"}, 
-        {name: "Coding"}
+        { name: "Reading" },
+        { name: "Swimming" },
+        { name: "Coding" }
       ],
     }
   }
