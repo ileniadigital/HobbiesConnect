6364bd9c98abb1451c7d510b515c121147fc5ac4
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-03 06:26:25 +0000

    Added pending friend, sorted friends into pending and accepted

--- frontend/src/pages/FriendRequests.vue
+++ frontend/src/pages/FriendRequests.vue
@@ -3,18 +3,23 @@
     <div class="h1">
       {{ title }}
     </div>
-    <!-- Pending Requests -->
-    <div>
-      <h2>Pending Requests</h2>
-    </div>
-    <!-- My friends -->
-    <div>
-      <h2>My Friends</h2>
-      <ul>
-        <li v-for="friend in friends" :key="friend.id">
+    <hr />
+
+    <div class="row">
+      <!-- Pending Requests -->
+      <div class="col-md-6">
+        <h2>Pending Requests</h2>
+        <div v-for="request in pendingRequests" :key="request.id">
+          <Request :name="request.friend" :status="request.status" :createdAt="request.created_at" />
+        </div>
+      </div>
+      <!-- My friends -->
+      <div class="col-md-6">
+        <h2>My Friends</h2>
+        <div v-for="friend in acceptedRequests" :key="friend.id">
           <Request :name="friend.friend" :status="friend.status" :createdAt="friend.created_at" />
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,20 +36,32 @@ export default defineComponent({
   setup() {
     const mainStore = useMainStore();
     const friends = ref([]);
+    const pendingRequests = ref([]);
+    const acceptedRequests = ref([]);
 
     async function fetchFriends() {
       await mainStore.fetchData();
       friends.value = mainStore.friends;
-      console.log("Friends in list:", friends.value);
+      console.log("Friends:", friends.value);
+      // Sort friendships into accepted and pending
+      pendingRequests.value = friends.value.filter((f) => f.status === "PENDING");
+      console.log("Pending:", pendingRequests.value);
+      acceptedRequests.value = friends.value.filter((f) => f.status === "ACCEPTED");
+      console.log("Accepted:", acceptedRequests.value);
     }
     onMounted(fetchFriends);
 
     return {
       title: "Friend Requests",
-      friends,
+      pendingRequests,
+      acceptedRequests,
     };
   }
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.row {
+  margin-top: 2rem;
+}
+</style>
