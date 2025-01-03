621b59264bbfc4a499c178e100b8a708637d25b6
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-03 06:13:16 +0000

    Created foundation for FriendRequest page

---
+++ frontend/src/components/FriendRequests/Request.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ name }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">Status: {{ status }}</h6>
+            <p class="card-text">Friend since: {{ new Date(createdAt).toLocaleDateString() }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+    name: "Request",
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: String,
+            required: true
+        }
+    }
+});
+</script>
+
+<style scoped>
+.card {
+    margin-bottom: 1rem;
+}
+</style>
--- frontend/src/pages/FriendRequests.vue
+++ frontend/src/pages/FriendRequests.vue
@@ -3,21 +3,48 @@
     <div class="h1">
       {{ title }}
     </div>
+    <!-- Pending Requests -->
+    <div>
+      <h2>Pending Requests</h2>
+    </div>
+    <!-- My friends -->
+    <div>
+      <h2>My Friends</h2>
+      <ul>
+        <li v-for="friend in friends" :key="friend.id">
+          <Request :name="friend.friend" :status="friend.status" :createdAt="friend.created_at" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
+import { useMainStore } from "../data/data";
+import Request from "../components/FriendRequests/Request.vue";
 
 export default defineComponent({
   components: {
+    Request,
   },
-  data() {
+  setup() {
+    const mainStore = useMainStore();
+    const friends = ref([]);
+
+    async function fetchFriends() {
+      await mainStore.fetchData();
+      friends.value = mainStore.friends;
+      console.log("Friends in list:", friends.value);
+    }
+    onMounted(fetchFriends);
+
     return {
       title: "Friend Requests",
-    }
+      friends,
+    };
   }
-})
+});
 </script>
 
 <style scoped></style>
