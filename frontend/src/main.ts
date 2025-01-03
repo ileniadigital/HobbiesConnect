cbb1e4c38a99a4931d9810194b46a53c0ee3bd80
amy <amyanigboro@gmail.com>
2025-01-03 20:28:07 +0000

    changed to ajax
    the methods in profile used axios but now it uses ajax instead

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import axios from "axios";
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-
-axios.defaults.baseURL = "http://127.0.0.1:8000";
 
 const pinia = createPinia();
 const app = createApp(App);
--- frontend/src/pages/Profile.vue
+++ frontend/src/pages/Profile.vue
@@ -6,25 +6,25 @@
 
     <!-- Profile Form -->
     <div class="row">
-      <div class="col-md-12">
-        <form> <!-- @submit.prevent="updateUserProfile"  ADD BACK ONCE CHANGED TO AJAX-->
-          <div class="form-row vertical">
-            <div class="form-field">
+      <!-- <div class="col-md-12"> -->
+        <form @submit.prevent="updateUserProfile"> <!--   ADD BACK ONCE CHANGED TO AJAX-->
+          <div class="form-row">
+            <div class="form-group col-md-4">
               <label for="dob">Date of Birth:</label>
               <input type="date" v-model="dateofbirth" id="dob" />
             </div>
-            <div class="form-field">
+            <div class="form-group col-md-4">
               <label for="name">Name</label>
               <input type="text" v-model="name" id="name" />
             </div>
-            <div class="form-field">
+            <div class="form-group col-md-4">
               <label for="email">Email</label>
               <input type="email" v-model="email" id="email" />
             </div>
             <button type="submit" class="btn btn-primary">Update Profile</button>
           </div>
         </form>
-      </div>
+      <!-- </div> -->
     </div>
     <!-- Update Password -->
     <div class="updatepassword">
@@ -58,39 +58,55 @@ export default defineComponent({
       userId: 1, // temporary 
     };
   },
-  // methods: {
-  //   async fetchUserProfile() {
-  //     try {
-  //       const response = await axios.get(`/api/users/${this.userId}/`);
-  //       const user = response.data;
-  //       this.name = user.first_name + " " + user.last_name;
-  //       this.email = user.email;
-  //       this.dateofbirth = user.dob;
-  //     } catch (error) {
-  //       console.error("Error fetching user profile:", error);
-  //     }
-  //   },
-  //   async updateUserProfile() {
-  //     try {
-  //       const [firstName, ...lastNameParts] = this.name.split(" ");
-  //       const lastName = lastNameParts.join(" ");
-  //       const data = {
-  //         first_name: firstName,
-  //         last_name: lastName,
-  //         email: this.email,
-  //         dob: this.dateofbirth,
-  //       };
-  //       await axios.put(`/api/users/${this.userId}/`, data);
-  //       alert("Profile updated successfully!");
-  //     } catch (error) {
-  //       console.error("Error updating profile:", error);
-  //       alert("Failed to update profile.");
-  //     }
-  //   },
-  // },
-  // mounted() {
-  //   this.fetchUserProfile();
-  // },
+  methods: {
+    fetchUserProfile() {
+      const xhreq = new XMLHttpRequest();
+      xhreq.open("GET", `/api/users/${this.userId}/`, true);
+      xhreq.onload = () => {
+        if (xhreq.status === 200) {
+          const user = JSON.parse(xhreq.responseText);
+          this.name = user.first_name + " " + user.last_name;
+          this.email = user.email;
+          this.dateofbirth = user.dob;
+        } else {
+          console.error("Error fetching user profile:", xhreq.statusText);
+        }
+      };
+      xhreq.onerror = () => {
+        console.error("Network error while fetching user profile.");
+      };
+      xhreq.send();
+    },
+    updateUserProfile() {
+      const xhreq = new XMLHttpRequest();
+      xhreq.open("PUT", `/api/users/${this.userId}/`, true);
+      xhreq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhreq.onload = () => {
+        if (xhreq.status === 200) {
+          alert("Profile updated successfully!");
+        } else {
+          console.error("Error updating profile:", xhreq.statusText);
+          alert("Failed to update profile.");
+        }
+      };
+      xhreq.onerror = () => {
+        console.error("Network error while updating profile.");
+        alert("Failed to update profile.");
+      };
+      const [firstName, ...lastNameParts] = this.name.split(" ");
+      const lastName = lastNameParts.join(" ");
+      const data = JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        email: this.email,
+        dob: this.dateofbirth,
+      });
+      xhreq.send(data);
+    },
+  },
+  mounted() {
+    this.fetchUserProfile();
+  },
 });
 </script>
 
