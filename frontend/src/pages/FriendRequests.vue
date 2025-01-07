d07363318498a1e121baa7b23c7f7f5558374a12
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-07 22:23:51 +0000

    Revert "changed name on each page to be dynamic"
    This reverts commit 604b8df1d2415b957445a6dbe5c19fb648f6033a.

--- frontend/src/pages/FriendRequests.vue
+++ frontend/src/pages/FriendRequests.vue
@@ -3,6 +3,34 @@
     <div class="h1">
       {{ title }}
     </div>
+    <hr />
+
+    <div class="row">
+      <!-- Pending Requests -->
+      <div class="col-md-6">
+        <h2>Pending Requests</h2>
+        <ul class="list-unstyled">
+          <li v-for="request in pendingRequests" :key="request.id">
+            <Request :name="request.friend" :status="request.status" :createdAt="request.created_at"
+              @delete-request="openDeleteModal(request.id, request.status)" />
+          </li>
+        </ul>
+      </div>
+      <!-- My friends -->
+      <div class="col-md-6">
+        <h2>My Friends</h2>
+        <ul class="list-unstyled">
+          <li v-for="friend in acceptedRequests" :key="friend.id">
+            <Request :name="friend.friend" :status="friend.status" :createdAt="friend.created_at"
+              @delete-request="openDeleteModal(friend.id, friend.status)" />
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Delete Request Modal -->
+    <DeleteRequest :visible="isDeleteModalVisible" :userId="userId" :friendId="selectedFriendId"
+      :status="selectedStatus" @close="isDeleteModalVisible = false" @request-deleted="fetchFriends" />
   </div>
 </template>
 
@@ -15,7 +43,16 @@ export default defineComponent({
   data() {
     return {
       title: "Friend Requests",
-    }
+      name: mainStore.user.first_name,
+      pendingRequests,
+      acceptedRequests,
+      isDeleteModalVisible,
+      selectedFriendId,
+      selectedStatus,
+      userId,
+      openDeleteModal,
+      fetchFriends,
+    };
   }
 })
 </script>
