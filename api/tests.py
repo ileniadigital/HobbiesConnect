1852c341343db7b7d7427ad4a945a20e4024819b
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-10 00:50:47 +0000

    refreshed requirements.txt and added set up for selenium testing

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
-# Create your tests here.
+class HostTest(LiveServerTestCase):
+    def testProfile(self):
+        driver= webdriver.Chrome()
+        driver.get("http://localhost:5173/")
+        assert "ECS639 Web Programming - Group CW Template" in driver.title
