853dd0566c9fb5d99d7582b12e7c23deddcf16bf
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-12 00:38:30 +0000

    set up tests for sign up

--- api/tests.py
+++ api/tests.py
@@ -1,11 +1,13 @@
 from datetime import date
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.test import LiveServerTestCase
 from django.contrib.auth import get_user_model
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 from api.models import *
 import time
@@ -30,23 +32,64 @@ class HostTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_login(self):
+    # def test_login(self):
+    #     '''
+    #     Test user log in
+    #     '''
+    #     driver = self.driver
+    #     driver.get(self.live_server_url + '/login/')
+    #     # Find the username and password input elements
+    #     username_input = driver.find_element(By.NAME, 'username')
+    #     password_input = driver.find_element(By.NAME, 'password')
+
+    #     # Enter the username and password
+    #     username_input.send_keys('test@user.email')
+    #     password_input.send_keys('testPassword123')
+
+    #     # Submit the form
+    #     password_input.send_keys(Keys.RETURN)
+
+    #     # Check that the user is redirected to the profile page
+    #     time.sleep(3)
+    #     self.assertIn("Profile", driver.page_source)
+
+    def test_signup(self):
         '''
-        Test user log in
+        Test user sign up
         '''
         driver = self.driver
-        driver.get(self.live_server_url + '/login/')
-        # Find the username and password input elements
-        username_input = driver.find_element(By.NAME, 'username')
-        password_input = driver.find_element(By.NAME, 'password')
-
-        # Enter the username and password
-        username_input.send_keys('test@user.email')
-        password_input.send_keys('testPassword123')
+        driver.get(self.live_server_url + '/signup/')
+        
+        # Add explicit waits for the elements to be present
+        email_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'email'))
+        )
+        first_name_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'first_name'))
+        )
+        last_name_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'last_name'))
+        )
+        dob_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'dob'))
+        )
+        password_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'password1'))
+        )
+        password__confirmation_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'password2'))
+        )
+        # Enter the user details
+        email_input.send_keys('new@user.email')
+        first_name_input.send_keys('New')
+        last_name_input.send_keys('User')
+        dob_input.send_keys('1990-01-01')
+        password_input.send_keys('newPassword123')
+        password__confirmation_input.send_keys('newPassword123')
 
         # Submit the form
         password_input.send_keys(Keys.RETURN)
 
-        # Check that the user is redirected to the profile page
-        time.sleep(3)
-        self.assertIn("Profile", driver.page_source)
+        # Check that the user is redirected to the login page
+        time.sleep(5)
+        self.assertIn("Login", driver.page_source)
