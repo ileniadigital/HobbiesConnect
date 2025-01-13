88bc70015763448491d02a523dabd294e2db6327
Sandeep <SaiSandeepVemula@gmail.com>
2025-01-13 22:57:26 +0000

    Update auth.ts

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia';
 import { User } from './interfaces';
-import { getCsrfToken} from './csrf';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
@@ -10,15 +9,7 @@ export const useAuthStore = defineStore('auth', {
   }),
   actions: {
     async checkAuthentication(): Promise<boolean> {
-      try {
-      const csrfToken = getCsrfToken();
-      console.log('CSRF Token:', csrfToken);
-      const sessionId = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('sessionid='))
-        ?.split('=')[1];
-      console.log('Session ID:', sessionId);
-      
+      try {      
       alert('Calling fetch');
       const response = await fetch('http://localhost:8000/api/authenticated/', {
         credentials: 'include',
@@ -29,11 +20,11 @@ export const useAuthStore = defineStore('auth', {
       this.isAuthenticated = data.isAuthenticated;
 
       if (this.isAuthenticated) {
-        alert('You are logged in! Data will be fetched.');
+        // alert('You are logged in! Data will be fetched.');
         this.user = data.user;
         this.userid = data.user.id;
       } else { //logging out 
-        window.alert("You are not authenticated. Please log in.");
+        // window.alert("You are not authenticated. Please log in.");
         this.user = null;
         this.userid = null;
         window.location.href = ('http://localhost:8000/login');
@@ -44,13 +35,6 @@ export const useAuthStore = defineStore('auth', {
       console.error('Error checking authentication:', error);
       return false;
       }
-    },
-    clearCsrfToken() {
-      document.cookie.split(';').forEach(cookie => {
-        document.cookie = cookie
-          .replace(/^ +/, '')
-          .replace(/=.*/, `=;expires=${new Date(0).toUTCString()};path=/`);
-      });
     },
   },
   getters: {
