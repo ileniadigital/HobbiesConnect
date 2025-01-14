ddf18be987d661af4659e0e3fce31e3a33c0983a
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2025-01-14 22:11:41 +0000

    separated each data retrieved into its own function and added type annotations

--- frontend/src/data/data.ts
+++ frontend/src/data/data.ts
@@ -6,37 +6,62 @@ const BASE_URL = 'http://localhost:8000';
 
 export const useMainStore = defineStore('main', {
     state: () => ({
-        user: null as User | null, 
+        user: null as User | null,
         userId: null as number | null,
         hobbies: [] as Hobbies[],
         userHobbies: [] as UserHobby[],
-        friends: [] as Friendship[],
+        friends: [] as Friendship[], 
         isAuthenticated: false 
     }),
     actions: {
+        // Fetch all data
         async fetchData() {
             try {
-                // Fetch user data
                 const authStore = useAuthStore();
                 const isAuthenticated = await authStore.checkAuthentication();
                 if (!isAuthenticated) {
                     throw new Error('User is not authenticated');
                 }
                 this.userId = authStore.get_user_id;
-                console.log("User ID", this.userId);
+                // console.log("User ID", this.userId);
+
+                await this.fetchUser();
+                await this.fetchHobbies();
+                await this.fetchUserHobbies();
+                await this.fetchFriends();
+            } catch (error) {
+                console.error("Can't fetch initial data", error);
+            }
+        },
+        // Fetch user data
+        async fetchUser(): Promise<void> {
+            try {
                 const userResponse = await fetch(`${BASE_URL}/user/${this.userId}/`, { credentials: 'include' });
                 if (!userResponse.ok) {
                     throw new Error('Failed to fetch user data');
                 }
                 this.user = await userResponse.json() as User;
-                this.userId = this.user.id;
-                console.log("User data", this.user);
-
+                // console.log("User data", this.user);
+            } catch (error) {
+                console.error("Failed to fetch user data", error);
+            }
+        },
+        // Fetch hobbies data
+        async fetchHobbies(): Promise<void> {
+            try {
                 const hobbiesResponse = await fetch(`${BASE_URL}/api/hobbies/`, { credentials: 'include' });
-                this.hobbies = await hobbiesResponse.json();
-                console.log("Hobbies data", this.hobbies);
-
-                // Fetch user hobbies data
+                if (!hobbiesResponse.ok) {
+                    throw new Error('Failed to fetch hobbies data');
+                }
+                this.hobbies = await hobbiesResponse.json() as Hobbies[];
+                // console.log("Hobbies data", this.hobbies);
+            } catch (error) {
+                console.error("Failed to fetch hobbies data", error);
+            }
+        },
+        // Fetch user hobbies data
+        async fetchUserHobbies(): Promise<void> {
+            try {
                 const userHobbiesResponse = await fetch(`${BASE_URL}/user/${this.userId}/hobbies/`, { credentials: 'include' });
                 if (!userHobbiesResponse.ok) {
                     throw new Error('Failed to fetch user hobbies data');
@@ -51,26 +76,31 @@ export const useMainStore = defineStore('main', {
                         description: uh.description
                     }
                 })) as UserHobby[];
-                console.log("Hobbies user data", this.userHobbies);
-
-                // Fetch friends data
+                // console.log("Hobbies user data", this.userHobbies);
+            } catch (error) {
+                console.error("Failed to fetch user hobbies data", error);
+            }
+        },
+        // Fetch friends data
+        async fetchFriends(): Promise<void> {
+            try {
                 const friendshipResponse = await fetch(`${BASE_URL}/user/${this.userId}/friendships/`, { credentials: 'include' });
                 if (!friendshipResponse.ok) {
                     throw new Error('Failed to fetch friends data');
                 }
                 this.friends = await friendshipResponse.json() as Friendship[];
-                console.log("Friends data", this.friends);
+                // console.log("Friends data", this.friends);
             } catch (error) {
-                console.error("Can't fetch initial data", error);
+                console.error("Failed to fetch friends data", error);
             }
         }
     },
     getters: {
         get_user_id(): number | null {
-          return this.userId;
+            return this.userId;
         },
         get_user(): User | null {
-          return this.user;
+            return this.user;
         },
-      },
+    },
 });
